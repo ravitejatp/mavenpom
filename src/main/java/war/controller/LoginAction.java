8f564ee18d19bd1ae2645361de8c891682233e6a
raviteja <ravitej569@gmail.com>
2018-02-28 13:28:00 +0530

    added a new files

---
+++ src/main/java/war/controller/LoginAction.java
@@ -0,0 +1,41 @@
+package war.controller;
+
+import war.model.Usuario;
+import war.exceptions.UsuarioNAutorizado;
+
+public class LoginAction extends BaseAction{
+
+	private String login;
+	
+	private String senha;
+	
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String login(){
+		if (getLoginService().doLogin(getLogin(), getSenha())) {
+			return "valid";
+		}
+		else {
+			addActionError("INVALID LOGIN / PASSWORD");
+			return "invalid";
+		}
+		
+	}
+	
+	
+}
